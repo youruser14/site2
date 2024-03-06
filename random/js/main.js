a147129db634fa0f94ab9e87c92474f662be57ca
youruser14 <144472534+youruser14@users.noreply.github.com>
2024-03-06 12:16:17 +0300

    Add files via upload

---
+++ random/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instancesSidenav = M.Sidenav.init(sidenav);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.querySelectorAll('.modal');
+    var instancesModal = M.Modal.init(modal);
+  });
